eb7fdcffaf68c346cfc7b6390ca9b412d29668f7
Mark Shropshire <mdshrops@shropnet.net>
2012-02-20 07:00:25 -0500

    Updated iOS Home Screen title to be "DrupalCLT"

--- js/ios-title.js
+++ js/ios-title.js
@@ -2,5 +2,5 @@ if( navigator.userAgent.match(/iPhone/i) ||
 navigator.userAgent.match(/iPod/i) ||
 navigator.userAgent.match(/iPad/i)
 ) {
-document.title = "DrupalCampCLT"; // 16 characters or fewer (including spaces)
+document.title = "DrupalCLT"; // 16 characters or fewer (including spaces)
 }
