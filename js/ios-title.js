f98c52e070d293f0291765edcdaa3220e1cf5f98
Mark Shropshire <mdshrops@shropnet.net>
2012-02-20 06:56:27 -0500

    Added some javascript to allow a shorter title when the site is saved to an iOS home screen. Found code at http://studiohyperset.com/itouch-home-screen-short-title/2337.

---
+++ js/ios-title.js
@@ -0,0 +1,6 @@
+if( navigator.userAgent.match(/iPhone/i) ||
+navigator.userAgent.match(/iPod/i) ||
+navigator.userAgent.match(/iPad/i)
+) {
+document.title = "DrupalCampCLT"; // 16 characters or fewer (including spaces)
+}
